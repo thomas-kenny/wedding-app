e33196cac201398d22b25832ba11f003947d3338
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-03-24 19:17:15 +0000

    Map working

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -1,13 +1,15 @@
 class WeddingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :show ]
   def show
-  @wedding = Wedding.first
-  @rsvp = Rsvp.new
+    @wedding = Wedding.first
+    @rsvp = Rsvp.new
+    @weddings = Wedding.all
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-    @markers =
+    @markers = @weddings.geocoded.map do |wedding|
       {
-        lat: @wedding.latitude,
-        lng: @wedding.longitude
+        lat: wedding.latitude,
+        lng: wedding.longitude
       }
+    end
   end
 end
