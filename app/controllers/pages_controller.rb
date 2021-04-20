42dbe4d6ffd7a762c7fb743e6e732eb4c3cd5188
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-04-20 13:38:27 +0100

    Wedding info on one page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :gifts, :travel ]
   def home
     @wedding = Wedding.first
+    @rsvp = Rsvp.new
   end
 
   def gifts
